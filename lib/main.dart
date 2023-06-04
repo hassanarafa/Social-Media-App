534b693b84ec6a125428292a66bc48b9487bb000
hassanarafa <hassan.arafa.e@gmail.com>
2023-06-04 17:24:54 +0300

    create loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socialmediaapp/pages/LoginPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowMaterialGrid: false,
-      home: Scaffold(),
-    );
+        debugShowCheckedModeBanner: false, home: LgoinPage());
   }
 }
